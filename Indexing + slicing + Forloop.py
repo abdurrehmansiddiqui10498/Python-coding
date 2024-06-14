aa808f1d594302f51607a7a04e090d27703ecedd
Abdur Rehman Siddiqui <abisbah2011@gmail.com>
2024-06-14 19:18:02 +0500

    Indexing + slicing + Forloop.py
    The above is what I learned

---
+++ Indexing + slicing + Forloop.py
@@ -0,0 +1,25 @@
+#indexing
+
+a="Aiman"
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+print(a[5])
+
+#Forloop
+
+a="Aiman"
+for character in a :
+  print(character)
+
+#Slicing
+
+a="Aiman"
+print(a[0:4])
+print(a[1:3])
+print(a[2:5])
+print(a[3:2])
+print(a[4:])
+print(a[:5])
