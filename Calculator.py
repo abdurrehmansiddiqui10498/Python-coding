87636640871aa15df8d2b0019aa581558dd8909f
Abdur Rehman Siddiqui <abisbah2011@gmail.com>
2024-06-14 19:10:47 +0500

    Calculator.py
    This is a way to calculate the numbers

---
+++ Calculator.py
@@ -0,0 +1,9 @@
+#Calculator
+a=15
+b=19
+print("The addition of ",a,"and",b,"is",a+b)
+print("The Subtraction of ",a,"and",b,"is",a-b)
+print("The Multiplication of ",a,"and",b,"is",a*b)
+print("The Division of ",a,"and",b,"is",a/b)
+print("The Modulus of ",a,"and",b,"is",a%b)
+print("The Floor Division of ",a,"and",b,"is")
